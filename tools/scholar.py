1573689c15c6815eb0a3c326d664cf9c8418a361
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-08-04 01:18:58 +0330

    fix author page

--- tools/scholar.py
+++ tools/scholar.py
@@ -38,6 +38,6 @@ def get_author_dict(author_name: str):
 
 if __name__ == "__main__":
     # Example usage
-    author_name = "Mahdi Jafari Siavoshani"
+    author_name = "Keivan Rezaei"
     print(get_home_page(author_name))
     print(get_author_interests(author_name))
