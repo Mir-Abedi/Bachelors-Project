0745820d2f6fb998445b0d82e9afafde90598741
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-08-06 01:27:17 +0330

    change send mail

--- tools/email_sender.py
+++ tools/email_sender.py
@@ -2,12 +2,25 @@ import yagmail
 from utils import config
 import os
 from celery import shared_task
+import smtplib
+from email.mime.text import MIMEText
 
 @shared_task
 def send_email(body, subject, reciever):
-    gmail = config("GMAIL")
-    gmail_app_password = os.getenv("GMAIL_APP_PASSWORD")
-    if not gmail_app_password:
-        raise Exception("GMAIL_APP_PASSWORD not in env")
-    yag = yagmail.SMTP(gmail, gmail_app_password)
-    yag.send(to=reciever, subject=subject, contents=body)
+
+    sender = config("HOTMAIL_SENDER")
+    password = os.getenv("HOTMAIL_APP_PASSWORD")
+    if not password:
+        raise Exception("HOTMAIL_APP_PASSWORD not in env")
+
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = reciever
+
+    with smtplib.SMTP("smtp.office365.com", 587) as server:
+        server.starttls()
+        server.login(sender, password)
+        server.send_message(msg)
+
+    print("Email sent successfully.")
